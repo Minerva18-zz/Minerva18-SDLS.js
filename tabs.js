6f8bc96e9a05ab13891687c3c27427e63b22beb2
Raghavendhra Yarlagadda <raghavendhray@gmail.com>
2016-08-06 17:23:24 +0530

    Tabs

--- tabs.js
+++ tabs.js
@@ -1,7 +1,7 @@
 j$('.slds-tabs--default__link,.slds-tabs--scoped__link').click(function(){
-		j$(this).parent().parent().find('.slds-tabs--default__link,.slds-tabs--scoped__link').attr('aria-selected','false');
-		j$(this).attr('aria-selected','true');
-		j$(this).parent().addClass('slds-active').siblings().removeClass('slds-active');
-		j$(this).parent().parent().parent().find('.'+j$(this).parent().parent().parent().find('.slds-tabs--default__content,.slds-tabs--scoped__content')[0].classList[0]).removeClass('slds-show').addClass('slds-hide');
-		j$(this).parent().parent().parent().find('#'+j$(this).attr('aria-controls')).removeClass('slds-hide').addClass('slds-show');
+	j$(this).parent().parent().find('.slds-tabs--default__link,.slds-tabs--scoped__link').attr('aria-selected','false');
+	j$(this).attr('aria-selected','true');
+	j$(this).parent().addClass('slds-active').siblings().removeClass('slds-active');
+	j$(this).parent().parent().parent().find('.'+j$(this).parent().parent().parent().find('.slds-tabs--default__content,.slds-tabs--scoped__content')[0].classList[0]).removeClass('slds-show').addClass('slds-hide');
+	j$(this).parent().parent().parent().find('#'+j$(this).attr('aria-controls')).removeClass('slds-hide').addClass('slds-show');
 }); 
