095bbca9718104456a00708447791804a0972a1f
Raghavendhra Yarlagadda <raghavendhray@gmail.com>
2016-10-01 22:36:18 +0530

    Create toggle.js

---
+++ toggle.js
@@ -0,0 +1,7 @@
+/*
+The Toggle’s value is either true or false.
+We can get those values using the ‘checked’ property of the input of the toggle element.
+Like(in jQuery):
+*/
+var j$ = jQuery.noConflict();
+j$('#id_of_the_input_tag_of_the_toggle_element').prop('checked');
